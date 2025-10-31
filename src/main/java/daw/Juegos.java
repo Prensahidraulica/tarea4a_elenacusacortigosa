ec68fc685d329f47d9b99ed7e0ef1fc0653860b9
Elena <elenalapalmera17@gmail.com>
2025-10-31 13:56:43 +0100

    Pasillo, despensa, salón y biblioteca completos

--- src/main/java/daw/Juegos.java
+++ src/main/java/daw/Juegos.java
@@ -1,5 +1,6 @@
 package daw;
 
+import java.nio.channels.Pipe.SourceChannel;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -22,7 +23,7 @@ public class Juegos {
 
     public static String DecidirPuertaVentana() {
         Random numAleatorio = new Random();
-        int puertaOVentanaInt = numAleatorio.nextInt(1, 2);
+        int puertaOVentanaInt = numAleatorio.nextInt(1, 3);
         String puertaOVentana = "";
 
         switch (puertaOVentanaInt) {
@@ -39,13 +40,15 @@ public class Juegos {
 
     public static String CasoPuerta() {
 
+        Random numAleatorio = new Random();
+        int numAleatorio1 = numAleatorio.nextInt(1, 3);
         boolean repetir = false; 
         Scanner teclado = new Scanner(System.in);
 
         String enunciado = """
                 ------------------------------------------------------------------------------------------------------------
-                Por suerte o por desgracia, la única manera de entrar a la casa es por la puerta del salón llena de fluidos 
-                viscosos. ¿Serán las mucosidades de algún flipado?
+                Por suerte o por desgracia, la única manera de entrar a la casa es por la puerta principal de la casa, 
+                llena de fluidos viscosos. ¿Serán las mucosidades de algún flipado?
                 ------------------------------------------------------------------------------------------------------------
                 """;
 
@@ -54,23 +57,65 @@ public class Juegos {
 
         do {
             System.out.println("¿Quieres entrar por la puerta llena de fluidos viscosos, o crees que la situación te supera? (Entrar / Salir)");
-            String respuesta = teclado.nextLine();
+            String respuesta1 = teclado.nextLine();
 
-            if (respuesta.equalsIgnoreCase("entrar")) {
+            if (respuesta1.equalsIgnoreCase("entrar")) {
                 String enunciadoEntrar = """
                         ---------------------------------------------------------------------------------------------------------------
-                        Abres la puerta viscosa del salón. Cuesta un poco de abrir, pero está tan, pero tan viscosa que no haces ruido. 
-                        (Incluso hay bichos pegados que no han visto en la vida)
+                        Abres la puerta viscosa de la entrada y entras al recibidor. Cuesta un poco de abrir, pero está tan, pero tan 
+                        viscosa que no haces ruido. (Incluso hay bichos pegados que no se han visto en la vida).
 
-                        El salón está repleto de muebles antiguos, aunque algunos están roidos y manchados de sangre. 
-                        Da la sensación de que la casa era de una familia de alta cuna. La única iluminación que hay es 
-                        la de los desgastados candelabros y del gran candil del techo, del cual cae una cortina desgastada
-                        ------------------------------------------------------------------------------------------------------------
+                        Hay varios cuadros en las paredes. Uno de ellos, el más grande y destacable, es "La consagración de Napoleón".
+                        Siento que el ovispo que sujeta la enorme cruz que está a la derecha de Napoleón me sigue con la mirada
+                        ---------------------------------------------------------------------------------------------------------------
                         """;
 
+                System.out.println();
                 System.out.println(enunciadoEntrar);
+                System.out.println();
+
+                do {
+                    System.out.println("Ahora puedes elegir entre abrir la puerta mugrienta del salón, la puerta colorida de la biblioteca o salir de la casa");
+                    System.out.println("Salón / Biblioteca / Salir");
+                    String respuesta2 = teclado.nextLine();
+
+                    if (respuesta2.equalsIgnoreCase("salón")) {
+                        String enunciadoSalon = """
+                                --------------------------------------------------------------------------------------------------
+                                El salón está repleto de muebles antiguos, aunque algunos están roidos y manchados de sangre. 
+                                Da la sensación de que la casa era de una familia de alta cuna. La única iluminación que hay es 
+                                la de los desgastados candelabros y del gran candil del techo, del cual cae una cortina desgastada
+                                --------------------------------------------------------------------------------------------------
+                                """;
+    
+                        System.out.println(enunciadoSalon);
+    
+                    } else if (respuesta2.equalsIgnoreCase("biblioteca")) {
+                        String enunciadoBiblioteca = """
+                                -----------------------------------------------------------------------------------------------------
+                                La biblioteca está llena de libros muy antiguos y polvorientos, algunos son de la época de Napoleón.
+                                El suelo está lleno de libros, todos abiertos por alguna página, y muchas hojas color café. Parece 
+                                como si el último dueño se pasara las noches en la biblioteca creando hipótesis paranóicas o haciendo
+                                un plan importante. 
+                                -----------------------------------------------------------------------------------------------------
+                                 """;
+                        System.out.println(enunciadoBiblioteca);
+                    
+                    } else if (respuesta2.equalsIgnoreCase("salir")) {
+                        System.out.println("Has decidido salir corriendo de la casa.");
+                        break;
+
+                    } else {
+                        System.out.println();
+                        System.out.println("- Error al introducir el dato. Recuerda poner la tilde a salón -");
+                        repetir = true;
+                    }
+                    
+                } while (repetir);
+
+
                 
-            } else if (respuesta.equalsIgnoreCase("salir")){
+            } else if (respuesta1.equalsIgnoreCase("salir")){
                 System.out.println("----------------------------------------------------------------------");
                 System.out.println();
                 System.out.println("Has decidido salir. ");
@@ -79,7 +124,7 @@ public class Juegos {
             } else {
                 System.out.println("----------------------------------------------------------------------");
                 System.out.println();
-                System.out.println("Has introducido mal tu respuesta");
+                System.out.println("- Has introducido mal tu respuesta, vuelve a introducirla -");
                 repetir = true;
             }
         } while (repetir);
@@ -112,14 +157,60 @@ public class Juegos {
             if (respuesta.equalsIgnoreCase("entrar")) {
                 String enunciadoEntrar = """
                         ------------------------------------------------------------------------------------------------------------
-                        Abres la puerta viscosa del salón. Cuesta un poco de abrir, pero está tan, pero tan viscosa que no haces ruido. 
-                        (Incluso hay bichos pegados que no han visto en la vida)
+                        Entras por la ventana a duras penas, te haces un corte en la pierna. Reza a Dios para que ese corte no se 
+                        infecte. Ves que toda la cocina está muy revuelta, toda la vajilla está en la pila con comida y roña seca, 
+                        hay vasos rotos y cubiertos por todo el suelo y una mesa y varias puertas de armarios caídas por el moho 
+                        blanco
 
-                        El salón está repleto de muebles antiguos, aunque algunos están roidos y manchados de sangre. 
-                        Da la sensación de que la casa era de una familia de alta cuna. La única iluminación que hay es 
-                        la de los desgastados candelabros y del gran candil del techo, del cual cae una cortina desgastada
+                        Todo el suelo está lleno de un líquido muy oscuro y pegajoso, como una mezcla entre granate y un marrón café.
+                        Da la sensación de que alguno de los vasos rotos del suelo contendría un líquido de un color parecido.
+                        Hay que reconocer que heule especialmente mal, todo está lleno de moscas
                         ------------------------------------------------------------------------------------------------------------
                         """;
+
+                        System.out.println(enunciadoEntrar);
+                        System.out.println();
+
+                        do {
+                            System.out.println("Ahora puedes elegir entre entrar a la despensa, ir al pasillo o salir de la casa");
+                            System.out.println("(Despensa / Pasillo / Salir)");
+                            String respuesta2 = teclado.nextLine();
+        
+                            if (respuesta2.equalsIgnoreCase("despensa")) {
+                                String enunciadoSalon = """
+                                        --------------------------------------------------------------------------------------------------
+                                        La despensa está completamente vacía, no hay nada de comida, como si la persona que vivía aquí
+                                        supiera que ya no iba a volver a vivir aquí nunca más. 
+
+                                        Lo único que hay son dos baldosas para apoyar la comida
+                                        --------------------------------------------------------------------------------------------------
+                                        """;
+                                
+                                System.out.println(enunciadoSalon);
+                                System.out.println();
+            
+                            } else if (respuesta2.equalsIgnoreCase("pasillo")) {
+                                String enunciadoBiblioteca = """
+                                        --------------------------------------------------------------------------------------------------------
+                                        En el pasillo hay un silencio muy raro, y hay una oscuridad que hace que no quieras mirar atrás. Se 
+                                        supone que la casa, por su antigüedad, no tiene ninguna instalación eléctrica, pero al final del pasillo
+                                        se ve una tenue luz blanca que no es posible crear sin electricidad.  
+                                        --------------------------------------------------------------------------------------------------------
+                                         """;
+                                System.out.println(enunciadoBiblioteca);
+
+                            } else if (respuesta2.equalsIgnoreCase("salir")) {
+                                System.out.println("Has decidido salir corriendo de la casa.");
+                                break;
+                    
+                            } else {
+                                System.out.println("-------------------------------------------------------------");
+                                System.out.println("Error al introducir el dato. Recuerda poner la tilde a salón");
+                                System.out.println("-------------------------------------------------------------");
+                                repetir = true;
+                            }
+                            
+                        } while (repetir);
                 
             } else if (respuesta.equalsIgnoreCase("salir")){
                 System.out.println("----------------------------------------------------------------------");
--- src/main/java/daw/Main.java
+++ src/main/java/daw/Main.java
@@ -2,36 +2,31 @@ package daw;
 
 public class Main {
     public static void main(String[] args) {
-        
-        // Juego "La casa del terror"
-        // Al iniciar el juego, el espíritu de la casa informa al usuario del sufrimiento que va a padecer si entra y lo asusta para que no lo haga
-        // Aleatoriamente, el espíritu decide abrir una puerta o una ventana de la casa y se lo indica al jugador.
-        // En este momento le pregunta si realmente quiere entrar o está tan aterrado que prefiere salir. 
 
         /* 
+         * Juego "La casa del terror"
+         * Al iniciar el juego, el espíritu de la casa informa al usuario del sufrimiento que va a padecer si entra y lo asusta para que no lo haga
+         * Aleatoriamente, el espíritu decide abrir una puerta o una ventana de la casa y se lo indica al jugador.
+         * En este momento le pregunta si realmente quiere entrar o está tan aterrado que prefiere salir. 
+         * 
          * Si decide entrar y lo hace por la puerta, el usuario avanza hasta el recibidor 
          * de la casa y debe elegir si quiere abrir la puerta de la biblioteca o la puerta del salón.
-         *  En cada una de estas estancias pueden pasar dos situaciones, una siempre es tenebrosa y en la otra consigue escapar de la casa. 
+         * En cada una de estas estancias pueden pasar dos situaciones, una siempre es tenebrosa y en la otra consigue escapar de la casa.
          * Por ejemplo, si decide entrar en la biblioteca puede pasar que salga un grupo de zombis del suelo buscándote 
-         * para comerte o que encuentres un agujero en el suelo que te permite escapar de la casa. Si decide entrar al salón 
+         * para comerte o que encuentres un agujero en el suelo que te permite escapar de la casa. Si decide entrar al salón
          * puede pasar que venga Frankenstein con intención de estrangularte o que haya una escalera en la chimenea y consigas escapar...
+         * 
+         * Si el jugador decide entrar por la ventana, el usuario llega a la cocina de la casa y debe elegir si quiere abrir la puerta de despensa 
+         * o la puerta que da al pasillo. En cada una de estas estancias pueden pasar dos situaciones, una siempre es tenebrosa y la otra es que
+         * consigues escapar. Inventa tus propias situaciones. Dale un toque personal al juego.
+         * 
+         * En cualquier caso, al entrar en una estancia el juego elige aleatoriamente entre una de las dos posibles situaciones. 
+         * Si surge la situación tenebrosa, entonces el espíritu de la casa aparece para jugar contigo. 
+         * El maligno pensará un número entre 1 y 20 (aleatoriamente) y le dará 3 vidas al jugador para adivinarlo. 
+         * Si el jugador lo adivina antes de las tres rondas, entonces libera a la casa del espíritu y se proclama héroe del pueblo. 
+         * En caso contrario, el espíritu maligno devuelve al jugador a la posición inicial para seguir torturándolo.
          */
-
-         /* 
-          * Si el jugador decide entrar por la ventana, el usuario llega a la cocina de la casa y debe elegir si quiere abrir la puerta de despensa 
-          * o la puerta que da al pasillo. En cada una de estas estancias pueden
-          * pasar dos situaciones, una siempre es tenebrosa y la otra es que
-          * consigues escapar. Inventa tus propias situaciones. Dale un toque personal al juego.
-          */
-
-        /* 
-          * En cualquier caso, al entrar en una estancia el juego elige aleatoriamente entre una de las dos posibles situaciones. 
-          * Si surge la situación tenebrosa, entonces el espíritu de la casa aparece para jugar contigo. 
-          * El maligno pensará un número entre 1 y 20 (aleatoriamente) y le dará 3 vidas al jugador para adivinarlo. 
-          * Si el jugador lo adivina antes de las tres rondas, entonces libera a la casa del espíritu y se proclama héroe del pueblo. 
-          * En caso contrario, el espíritu maligno devuelve al jugador a la posición inicial para seguir torturándolo.
-          */
-
+  
         boolean salir = false;
         
         System.out.println(Juegos.MensajeInicial);
@@ -39,8 +34,8 @@ public class Main {
 
         if (puertaOVentana.equalsIgnoreCase("puerta")) {
             String casoPuerta = Juegos.CasoPuerta();
-        } else {
-            
+        } else if (puertaOVentana.equalsIgnoreCase("ventana")) {
+          String casoPuerta = Juegos.CasoVentana();
         }
     }
 }
